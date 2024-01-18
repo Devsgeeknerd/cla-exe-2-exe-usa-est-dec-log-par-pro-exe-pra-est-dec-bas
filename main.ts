7cb2cd138ea3bd7c351e35f178682c42ddb3405b
Paulo <vanderley.1109@gmail.com>
2024-01-18 15:21:42 -0300

    adicionando conteudo

---
+++ main.ts
@@ -0,0 +1,8 @@
+let verificarPar: number = 10;
+let restoDivisao: number = verificarPar % 2;
+
+if (restoDivisao == 0) {
+  console.log(verificarPar);
+} else {
+  console.log("Número não é par!");
+}
